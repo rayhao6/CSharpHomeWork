9ca9764fc59999a5d101202fe5f40bb0159be4f3
Rayhao <rayhao@outlook.com>
2020-03-19 21:38:03 +0800

    work4

---
+++ Homework4/work4/timer/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Timer
+{
+    public delegate void AlamrHandler(object sender, AlarmEventArgs args);
+    public class AlarmEventArgs
+    {
+        public long CurrentTime;
+        public AlarmEventArgs(Timer alarm)
+        {
+            CurrentTime = alarm.CurrentTime;
+        }
+    }
+
+    public class Timer
+    {
+        public long CurrentTime;
+        public long Begin;
+        public long Clock;
+
+        public event AlamrHandler TickEvent;
+        public event AlamrHandler AlarmEvent;
+
+        public Timer()
+        {
+            CurrentTime = 0;
+            Begin = -1;
+            Clock = 0;
+        }
+
+        public void SetClock(long second)
+        {
+            Begin = CurrentTime;
+            Clock = second;
+        }
+
+        public void Start()
+        {
+            Console.Write("Start\n");
+            Console.Write("When do you want to be alarmed?");
+            string input = Console.ReadLine();
+            long Clocknum;
+            try
+            {
+                Clocknum = Int64.Parse(input);
+                if (Clocknum > 0) Clock = Clocknum;
+                else Clock = 5;
+            }
+            catch (Exception e) { Console.Write(e); }
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                CurrentTime++;
+                TickEvent(this, new AlarmEventArgs(this));
+                if ((CurrentTime - Begin) % Clock == 0)
+                {
+                    AlarmEvent(this, new AlarmEventArgs(this));
+                }
+            }
+
+        }
+    }
+    public class program
+    {
+        public Timer alarm = new Timer();
+        void Tick(object sender, AlarmEventArgs args)
+        {
+            Console.Write("Now:" + args.CurrentTime + '\n');
+        }
+        void Alarm(object sender, AlarmEventArgs args)
+        {
+            Console.Write("Alarm:" + (args.CurrentTime+1) + '\n');
+        }
+
+        public program()
+        {
+            alarm.TickEvent += Tick;
+            alarm.AlarmEvent += Alarm;
+        }
+    }
+    class run
+    {
+        static void Main(string[] args)
+        {
+            program one = new program();
+            one.alarm.Start();
+
+        }
+    }
+}
---
+++ Homework4/work4/work4/list.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T>action)
+        {
+            for(Node<T>x= head;x!=null;x = x.Next)
+            {
+                action(x.Data);
+            }
+        }
+
+    }
+
+    class list
+    {
+
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            //for (Node<int> node = intlist.Head;
+            //      node != null; node = node.Next)
+            //{
+            //    Console.WriteLine(node.Data);
+            //}
+            int max=0, min=999999999, sum=0;
+            intlist.ForEach(x => Console.WriteLine(x));
+            intlist.ForEach (x => sum += x);
+            intlist.ForEach(x => { if (x > max) max = x; });
+            intlist.ForEach(x => { if (x < min) min = x; });
+            Console.Write(sum);
+            Console.Write(max);
+            Console.Write(min);
+            // 字符串型List
+            GenericList<string> strList = new GenericList<string>();
+            //for (int x = 0; x < 10; x++)
+            //{
+            //    strList.Add("str" + x);
+            //}
+            //for (Node<string> node = strList.Head;
+            //        node != null; node = node.Next)
+            //{
+            //    Console.WriteLine(node.Data);
+            //}
+
+
+        }
+
+    }
+}
