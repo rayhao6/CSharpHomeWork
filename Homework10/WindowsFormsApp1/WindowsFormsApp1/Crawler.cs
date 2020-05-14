4b7c7d6c989a5287b4812c09b198037d36a24083
Rayhao <rayhao@outlook.com>
2020-05-14 08:53:22 +0800

    Homework10

---
+++ Homework10/WindowsFormsApp1/WindowsFormsApp1/Crawler.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrawlerForm
+{
+
+
+    class Crawler
+    {
+        public ConcurrentBag<UrlStates> urls = new ConcurrentBag<UrlStates>();
+        public int count = 0;
+        static public string startUrl = "";
+        static public string startWith = "";
+        public event Action<Crawler, UrlStates> PageDownloaded;
+        public void Crawl()
+        {
+            UrlStates surl = new UrlStates() { url = startUrl, processing = false, html = "" };
+            urls.Add(surl);
+
+            string str = @"(www\.){0,1}.*?\..*?/";
+            Regex r = new Regex(str);
+            Match m = r.Match(startUrl);
+            startWith = m.Value;
+
+            while (true)
+            {
+                UrlStates current = null;
+                foreach (var url in urls)
+                {
+                    if (url.processing) continue;
+                    current = url;
+                    if (count > 20)
+                        break;
+                    if (current == null)
+                        continue;
+                    current.processing = true;
+                    var t = new Thread(() => Process(current));
+                    t.Start();
+                    count++;
+                }
+            }
+        }
+
+        public bool UrlExists(string url)
+        {
+
+            foreach (UrlStates u in urls)
+            {
+                if (u.url == url)
+                    return true;
+            }
+            return false;
+        }
+
+
+        public void Process(UrlStates url)
+        {
+            try
+            {
+
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url.url);
+                string fileName = count.ToString();
+                File.WriteAllText(fileName, html, Encoding.UTF8);
+                url.html = html;
+                PageDownloaded(this, url);
+                Parse(html, url.url);//解析,并加入新的链接
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void Parse(string html, string oldUrl)
+        {
+
+            //匹配不含相对路径,且包含html的网址
+            string strRef = @"(href|HREF)[ ]*=[ ]*[""'](http|https)[^""'#>]+..html.*?[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0)
+                    continue;
+                //仅包含起始网站上的网页
+                if (url.Contains(startWith))
+                {
+                    if (!UrlExists(url))
+                    {
+                        urls.Add(new UrlStates() { url = url, processing = false, html = "" });
+                    }
+
+                }
+            }
+
+
+            strRef = @"(href|HREF)[ ]*=[ ]*[""'][^(http|https)][^""'#>]+..html.*?[""']";
+            matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                var url = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\"', '#', '>');
+                if (url.Length == 0) continue;
+                Uri baseUri = new Uri(oldUrl);
+                Uri absoluteUri = new Uri(baseUri, url);
+                if (url.Contains(startWith))
+                {
+                    if (!UrlExists(url))
+                    {
+                        urls.Add(new UrlStates() { url = url, processing = false, html = "" });
+                    }
+                }
+            }
+        }
+    }
+    class UrlStates
+    {
+        public string url { get; set; }    
+        public bool processing { get; set; }
+        public string html { get; set; }
+
+    }
+}
+
---
+++ Homework10/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.GridView1 = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.flowLayoutPanel1.Controls.Add(this.txtUrl);
+            this.flowLayoutPanel1.Controls.Add(this.btnStart);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(48, 361);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(27, 25, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1417, 76);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(31, 29);
+            this.txtUrl.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(548, 25);
+            this.txtUrl.TabIndex = 4;
+            this.txtUrl.Text = "http://www.baidu.com";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(587, 29);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 29);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // GridView1
+            // 
+            this.GridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.GridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.GridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridView1.Location = new System.Drawing.Point(62, 34);
+            this.GridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GridView1.Name = "GridView1";
+            this.GridView1.RowHeadersWidth = 51;
+            this.GridView1.RowTemplate.Height = 27;
+            this.GridView1.Size = new System.Drawing.Size(656, 271);
+            this.GridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.DataGridView GridView1;
+    }
+}
+
